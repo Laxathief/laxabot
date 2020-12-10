1bf1208afefc5d7b6254a21af7f3f71371a0ff31
Glitch (hello-webpage) <none>
2020-12-10 17:31:55 +0000

    🚞🏯 Checkpoint ./index.html:26118254/1974 ./script.js:26118254/915

--- script.js
+++ script.js
@@ -3,3 +3,37 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+element.style {
+}
+body #page-container .et_pb_section .et_pb_button_2 {
+    color: #000000!important;
+    border-radius: 40px;
+    font-family: 'Rubik',Helvetica,Arial,Lucida,sans-serif!important;
+}
+.et_pb_button_module_wrapper>a {
+    display: inline-block;
+
+}
+.et_pb_button_2, .et_pb_button_2:after {
+    transition: all 300ms ease 0ms;
+}
+.et_pb_button_2 {
+    box-shadow: 0px 2px 18px 0px rgba(0,0,0,0.3);
+}
+.et_pb_button, .et_pb_button_inner {
+    position: relative;
+}
+.et_pb_button {
+    font-size: 20px;
+    font-weight: 500;
+    padding: .3em 1em;
+    line-height: 1.7em!important;
+    background-color: transparent;
+    background-size: cover;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    border: 2px solid;
+    border-radius: 3px;
+    -webkit-transition: all .2s;
+    transition: all .2s;
+}

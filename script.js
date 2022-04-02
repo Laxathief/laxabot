38a0e51210b4e6197ac9053fb2ae9416ba496170
Glitch (hello-webpage) <none>
2022-04-02 22:06:51 +0000

    🐵🎯 Checkpoint ./index.html:26118254/488 ./script.babel:26118254/435 ./script.js:26118254/431 ./style.css:26118254/3429 ./live2d/index.js:26118254/156283 ./live2d/live2d.min.js:26118254/129055 ./live2d/live2dcubismcore.min.js:26118254/369789 ./live2d/pixi.min.js:26118254/402872

---
+++ script.js
@@ -0,0 +1,18 @@
+const cubism4Model =
+"https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json";
+
+(async function main() {
+  const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    resizeTo: window });
+
+
+  const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
+
+  app.stage.addChild(model4);
+
+  model4.scale.set(0.25);
+
+  model4.x = 300;
+})();

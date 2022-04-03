f8a4ea362e1dc3565eaf6e8fe075d492135a4568
Glitch (hello-webpage) <none>
2022-04-03 22:52:15 +0000

    ⚡️🕰 Checkpoint ./script.js:26118254/831

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-const cubism4Model =
-"./Live2D/mk1.model3.json";
+const cubism4Model = "./Live2D/mk1.model3.json";
 
 (async function main() {
   const app = new PIXI.Application({
     view: document.getElementById("canvas"),
     autoStart: true,
-    resizeTo: window });
+    resizeTo: window,
+  });
 
   const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
 
   app.stage.addChild(model4);
 
-  
-  model4.width=screen.width;
+  model4.width = window.innerWidth;
   model4.scale.set(model4.scale.x);
-
-
-  
 })();
-
